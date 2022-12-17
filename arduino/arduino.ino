8b538543b7f900c9be6eb8450fb0e57f6ea9afaa
Roberto Paes <robertocpaes@gmail.com>
2022-12-17 17:54:34 -0300

    change arduino protocol

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,38 +1,65 @@
-// https://www.dobitaobyte.com.br/socket-server-com-esp8266/
+/***
+Exemplo de https://josecintra.com/blog/comunicacao-websockets-nodemcu-esp8266/
+***/
+
 #include <ESP8266WiFi.h>
+#include <WebSocketsServer.h>
 #include "config.h"
 
-WiFiServer sockServer(SOCK_PORT);
+//Correção nivel lógico invertido
+#define ON LOW
+#define OFF HIGH
 
-void setup()
-{
-    Serial.begin(9600);
-    delay(1000);
-    WiFi.begin(SSID, PASSWD);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(100);
-    }
+WebSocketsServer webSocket = WebSocketsServer(SOCK_PORT); // Recebe dados do cliente
 
-    Serial.print("IP: ");
-    Serial.println(WiFi.localIP());
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
 
-    sockServer.begin();
-}
-void loop()
-{
-        WiFiClient client = sockServer.available();
-        if (client)
-        {
-            while (client.connected())
-            {
-                while (client.available() > 0)
-                {
-                    uint8_t value = client.read();
-                    Serial.write(value);
-                }
-                delay(10);
-            }
-            client.stop();
+  switch (type) {
+    case WStype_DISCONNECTED:
+      break;
+
+    case WStype_CONNECTED:
+      { IPAddress ip = webSocket.remoteIP(num);
+        Serial.println(ip);
+      }
+      break;
+
+    case WStype_TEXT:
+      { String text = String((char *) &payload[0]);
+        Serial.println(text);
+        Serial.println(num);
+        Serial.println(type);
+
+        if (text == "ON") {
+          digitalWrite(LED_BUILTIN, ON);
+        } else {
+          digitalWrite(LED_BUILTIN, OFF);
         }
+      }
+      break;
+
+  }
+
+}
+
+void setup() {
+  
+  // Inicialização do LED
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, OFF);
+  // Conexões wi-fi e websocket
+  WiFi.begin(SSID, PASSWD);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(". ");
+    delay(100);
+  }
+  Serial.println(WiFi.localIP());
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+
+}
+
+void loop() {
+  webSocket.loop();
 }
